2e769d4781ba90105c8b4cf5649ada2da56d58a7
Danilo Deademe <130806476+DaniloDeademe@users.noreply.github.com>
2025-11-13 08:33:12 -0300

    Implementação do sistema de gerenciamento de filas com modelos, serviços e rotas.

---
+++ queue/index.js
@@ -0,0 +1,9 @@
+const Queue = require('./structures/Queue');
+const IQueue = require('./structures/IQueue');
+const Nodes = require('./structures/Nodes');
+
+module.exports = {
+    Queue,
+    IQueue,
+    Nodes
+};
---
+++ queue/structures/Nodes.js
@@ -0,0 +1,8 @@
+class Nodes {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+module.exports = Nodes;
